6f4f5b6be914c1af4a613507e8e49b341d92077b
Giyoon Park <workspaceforict@gmail.com>
2021-05-29 19:54:34 +0900

    Update: camera setting layout

--- src/pages/MyPageUserCameraSetting.js
+++ src/pages/MyPageUserCameraSetting.js
@@ -1,8 +1,16 @@
-import { Typography } from "@material-ui/core";
+import {
+  Button,
+  Container,
+  CssBaseline,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import React, { useState } from "react";
 
 function MyPageUserCameraSetting() {
-  const [inputList, setInputList] = useState([{ serialNumber: "", location: "" }]);
+  const [inputList, setInputList] = useState([
+    { serialNumber: "", location: "" },
+  ]);
 
   // handle input change
   const handleInputChange = (e, index) => {
@@ -25,39 +33,70 @@ function MyPageUserCameraSetting() {
   };
 
   return (
-    <div>
-        <Typography variant="h4">
-            CCTV 추가하기
-        </Typography>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Typography variant="h4" style={{ marginTop: "40px" }}>
+        CCTV 추가하기
+      </Typography>
       {inputList.map((x, i) => {
         return (
-          <div>
-            <input
+          <div style={{ marginTop: "40px" }}>
+            <TextField
+              variant="outlined"
+              margin="normal"
               name="serialNumber"
+              required
               placeholder="시리얼 번호를 입력하세요"
               value={x.serialNumber}
               onChange={(e) => handleInputChange(e, i)}
+              fullWidth
             />
-            <input
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
               name="location"
               placeholder="CCTV 위치를 입력하세요"
               value={x.location}
               onChange={(e) => handleInputChange(e, i)}
+              fullWidth
             />
-            <div>
-              {inputList.length !== 1 && (
-                <button onClick={() => handleRemoveClick(i)}>
-                  Remove
-                </button>
-              )}
-              {inputList.length - 1 === i && (
-                <button onClick={handleAddClick}>Add</button>
-              )}
-            </div>
+            {inputList.length !== 1 && (
+              <>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{ marginTop: "20px" }}
+                  onClick={() => handleRemoveClick(i)}
+                >
+                  삭제하기
+                </Button>
+                <span style={{ marginLeft: "20px" }} />
+              </>
+            )}
+            {inputList.length - 1 === i && (
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ marginTop: "20px" }}
+                onClick={handleAddClick}
+              >
+                추가하기
+              </Button>
+            )}
           </div>
         );
       })}
-    </div>
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        fullWidth
+        style={{ marginTop: "40px" }}
+      >
+        저장하기
+      </Button>
+    </Container>
   );
 }
 
